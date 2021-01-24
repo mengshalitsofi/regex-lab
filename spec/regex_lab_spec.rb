bdf4eb84e69fad3e5dd8c733738282053206c4a5
mengshalitsofi <mengshalitsofi@gmail.com>
2021-01-24 10:48:40 -0800

    commit tree a2efc560c356906a2cf32b28810f40c530fd9a91

--- spec/regex_lab_spec.rb
+++ spec/regex_lab_spec.rb
@@ -11,9 +11,10 @@ describe "Working with Regular expressions" do
     end
 
     it "returns false for words starting with a consonant" do
-      do_not_match = %w{ chandoo Crenel crooked Flimsy folksy Dog Hermit
+     do_not_match = %w{ chandoo Crenel crooked Flimsy folksy Dog Hermit
      makeshift Palazzi sixfold Truth }
-
+     
+     
       do_not_match.each do |word|
         expect(starts_with_a_vowel?(word)).to be(false)
       end
@@ -22,7 +23,7 @@ describe "Working with Regular expressions" do
 
   describe "#words_starting_with_un_and_ending_with_ing" do
     it "returns an array with the words starting with 'un' and ending with 'ing'" do
-      words_string = "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary"
+      words_string = "auning unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary"
       
       expect(words_starting_with_un_and_ending_with_ing(words_string).count).to eq(4)
       expect(words_starting_with_un_and_ending_with_ing(words_string)).to include("understanding")
